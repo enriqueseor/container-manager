c78aff6c411e36447df9d3f984d738848ed19a54
enrique <enriqueseor@gmail.com>
2023-12-14 16:19:55 +0000

    more readable code

--- src/TestContenedores.java
+++ src/TestContenedores.java
@@ -20,8 +20,23 @@ public class TestContenedores {
     }
 
     private static int AskQuestion(){
-        String[] con={"salir", "Crear contenedor", "Modificar contenedor", "listar contenedores", "descargar barco"};
-        return JOptionPane.showOptionDialog(null, "Que quieres hacer", "BARCO CAPITÁN ENRIC", 0, JOptionPane.QUESTION_MESSAGE, null, con, "");
+        String[] con={
+                "salir",
+                "Crear contenedor",
+                "Modificar contenedor",
+                "listar contenedores",
+                "descargar barco"
+        };
+        return JOptionPane.showOptionDialog(
+                null,
+                "Que quieres hacer",
+                "BARCO CAPITÁN ENRIC",
+                JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                con,
+                ""
+        );
     }
 
     private static void CreateContenedor(Contenedor[] CO, int j, Mercancia[] ME){
@@ -30,6 +45,7 @@ public class TestContenedores {
         int i = CreateMercancia(ME, numserie);
         String estado;
         String[] s ={"abierto", "cerrado"};
+
         int state = JOptionPane.showOptionDialog(
                 null,
                 "¿Cual es el estado del contenedor?",
@@ -37,12 +53,23 @@ public class TestContenedores {
                 JOptionPane.YES_NO_OPTION,
                 JOptionPane.QUESTION_MESSAGE,
                 null, s,
-                "");
-        if (state == 0){ estado = "abierto"; }
-        else { estado = "cerrado"; } //si hubiere de escribirse podrias poner cualquier palabra
+                ""
+        );
+        if (state == 0){
+            estado = "abierto";
+        } else { estado = "cerrado"; }
 
         String[] opciones={"Seco","Refrigerado","Cisterna"};
-        int tipo = JOptionPane.showOptionDialog(null, "Tipo de contenedor", "Contenedores", 0, JOptionPane.QUESTION_MESSAGE, null, opciones, "Seco");
+        int tipo = JOptionPane.showOptionDialog(
+                null,
+                "Tipo de contenedor",
+                "Contenedores",
+                JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                opciones,
+                "Seco"
+        );
         switch (tipo) {
             case 0 -> { String color = JOptionPane.showInputDialog("¿Cual es el color");
                         CO[j] = new ContenedorSeco(numserie, capacidad, estado, color);
@@ -58,7 +85,12 @@ public class TestContenedores {
 
     private static int CreateMercancia(Mercancia[] ME, String numserie) {
         int i = 0;
-        int MER = JOptionPane.showConfirmDialog(null, "¿Quieres añadir una mercancia?", "Mercancias", JOptionPane.YES_NO_OPTION);
+        int MER = JOptionPane.showConfirmDialog(
+                null,
+                "¿Quieres añadir una mercancia?",
+                "Mercancias",
+                JOptionPane.YES_NO_OPTION
+        );
         String mercancia = "", cantidad = "";
         if ( MER == 0) {
             while (MER != 1) {
@@ -66,7 +98,12 @@ public class TestContenedores {
                 cantidad = JOptionPane.showInputDialog("Cantidad: ");
                 ME[i] = new Mercancia(numserie, mercancia, cantidad);
                 i++;
-                MER = JOptionPane.showConfirmDialog(null, "¿Quieres añadir otra mercancia?", "Mercancias", JOptionPane.YES_NO_OPTION);
+                MER = JOptionPane.showConfirmDialog(
+                        null,
+                        "¿Quieres añadir otra mercancia?",
+                        "Mercancias",
+                        JOptionPane.YES_NO_OPTION
+                );
             }
         }
         else {JOptionPane.showMessageDialog(null, "No has añadido ninguna mercancia");}
@@ -95,15 +132,35 @@ public class TestContenedores {
     private static int ElegirContenedor(Contenedor[] CO, int j){
         int mod = 1;
         int i = 99;
-        if (j == 0){JOptionPane.showMessageDialog(null, "El barco esta vacio");}
+        if (j == 0){
+            JOptionPane.showMessageDialog(
+                null,
+                "El barco esta vacio"
+            );
+        }
         else {
-            JOptionPane.showMessageDialog(null, "¿Que contenedor quieres modificar?");
+            JOptionPane.showMessageDialog(
+                    null,
+                    "¿Que contenedor quieres modificar?"
+            );
             while (mod != 0) {
                 while (i >= j) {
-                    i = Integer.parseInt(JOptionPane.showInputDialog("Introduce un numero entre 0 y " + j + "este ultimo excluido"));
+                    i = Integer.parseInt(
+                            JOptionPane.showInputDialog(
+                                    "Introduce un numero entre 0 y " + j + "este ultimo excluido"
+                            )
+                    );
                 }
-                JOptionPane.showMessageDialog(null, CO[i].toString());
-                mod = JOptionPane.showConfirmDialog(null, "¿Quieres modificar este barco?", "", JOptionPane.YES_NO_OPTION);
+                JOptionPane.showMessageDialog(
+                        null,
+                        CO[i].toString()
+                );
+                mod = JOptionPane.showConfirmDialog(
+                        null,
+                        "¿Quieres modificar este barco?",
+                        "",
+                        JOptionPane.YES_NO_OPTION
+                );
                 if (mod == 1) {i = 99;}
             }
         }
@@ -112,20 +169,31 @@ public class TestContenedores {
 
     private static void PrintContenedores(Contenedor[] CO, int j) {
         StringBuilder cadena = new StringBuilder();
-        if (j == 0){JOptionPane.showMessageDialog(null, "El barco esta vacio");}
+        if (j == 0){
+            JOptionPane.showMessageDialog(
+                null,
+                "El barco esta vacio"
+            );
+        }
         else {
             for (int i = 0; i < j; i++) {
                 cadena.append(CO[i]);
                 cadena.append("\n");
                 cadena.append("\n");
             }
-            JOptionPane.showMessageDialog(null, cadena);
+            JOptionPane.showMessageDialog(
+                    null,
+                    cadena
+            );
         }
     }
 
     private static int VaciarBarco(){
         int j = 0;
-        JOptionPane.showMessageDialog(null, "Se ha vaciado el barco");
+        JOptionPane.showMessageDialog(
+                null,
+                "Se ha vaciado el barco"
+        );
         return j;
     }
 }
